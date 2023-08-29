aaf5153d1fbb82d26e00e97625a3ac62099ab05f
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2023-08-29 10:04:30 -0400

    updated with clear humaneval

--- Cleared_Dataset/HumanEval/Files/69.py
+++ Cleared_Dataset/HumanEval/Files/69.py
@@ -1,6 +1,6 @@
 
 def search(lst) -> int:
-    '''
+    """
     You are given a non-empty list of positive integers. Return the greatest integer that is greater than 
     zero, and has a frequency greater than or equal to the value of the integer itself. 
     The frequency of an integer is the number of times it appears in the list.
@@ -11,4 +11,4 @@ def search(lst) -> int:
     3
     >>> search([5, 5, 4, 4, 4])
     -1
-    '''
+    """
