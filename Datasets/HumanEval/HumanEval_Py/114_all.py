d455c3d2dfa3cc1b77788ebe38c00377fc663771
Joy Saha <64152587+j-saha@users.noreply.github.com>
2023-10-04 00:59:43 +0600

    HumanEval python fixed

---
+++ Datasets/HumanEval/HumanEval_Py/114_all.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def minSubArraySum(nums: List[int]) -> int:
+    """
+    Given an array of integers numbers, find the minimum sum of any non-empty sub-array
+    of numbers.
+    Example
+    minSubArraySum([2, 3, 4, 1, 2, 4]) == 1
+    minSubArraySum([-1, -2, -3]) == -6
+    """
---
+++ Datasets/HumanEval/HumanEval_Py/131_all.py
@@ -0,0 +1,9 @@
+
+def prod_odd_digits(n: int) -> int:
+    """Given a positive integer n, return the product of the odd digits.
+    Return 0 if all digits are even.
+    For example:
+    prod_odd_digits(1)  == 1
+    prod_odd_digits(4)  == 0
+    prod_odd_digits(235) == 15
+    """
